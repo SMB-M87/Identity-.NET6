eb07698c57e69e4dac0c38283897062f43c6ae86
HenkLammers <s1164087@student.windesheim.nl>
2022-10-03 16:18:56 +0200

    First commit

---
+++ Identity6/Pages/Register.cshtml.cs
@@ -0,0 +1,52 @@
+using Identity6.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace Identity6.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [BindProperty]
+        public Register Register { get; set; }
+
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = Register.Email,
+                    Email = Register.Email
+                };
+
+                var result = await userManager.CreateAsync(user, Register.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return Page();
+        }
+    }
+}
